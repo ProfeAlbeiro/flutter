a478247cf89fa423852815e8bc75c44dbc364365
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-03-15 12:08:49 -0500

    Creando api

--- sources/api-rest-personas/index.mjs
+++ sources/api-rest-personas/index.mjs
@@ -3,25 +3,28 @@ import { createServer } from 'node:http';
 import express from 'express';
 import pool from './src/database/connection.mjs';
 
+
+// app.use(express.json());
+
 // Función para probar la conexión con la base de datos
 const testDBConnection = async () => {
-  try {
-    const [rows] = await pool.query('SELECT 1 AS result');
-    console.log('Conexión exitosa a la base de datos:', rows);
-  } catch (error) {
-    console.error('Error al conectar con la base de datos:', error.message);
-    process.exit(1); // Termina el proceso si hay error de conexión
-  }
+    try {
+        const [rows] = await pool.query('SELECT 1 AS result');
+        console.log('Conexión exitosa a la base de datos:', rows);
+    } catch (error) {
+        console.error('Error al conectar con la base de datos:', error.message);
+        process.exit(1); // Termina el proceso si hay error de conexión
+    }
 };
 
 const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!\n');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World!\n');
 });
 
 // starts a simple http server locally on port 3000
 server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
+    console.log('Listening on 127.0.0.1:3000');
 });
 
 // Verificar la conexión a la base de datos antes de iniciar el servidor
